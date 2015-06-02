53e03109bb4422cf1be660bff8907d75262bee7a
Trevor W. Schadt <trevor.schadt@gmail.com>
2015-06-01 22:16:10 -0400

    Creation of RestaurantReviews project

---
+++ RestaurantReviews/Controllers/ReviewsController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantReviews.Models;
+
+namespace RestaurantReviews.Controllers
+{
+    [RoutePrefix("api/reviews")]
+    public class ReviewsController : ApiController
+    {
+        private RestaurantReviewsEntities db = new RestaurantReviewsEntities();
+
+        #region REST-standard methods
+
+        // GET: api/Reviews
+        public IQueryable<Review> GetReviews()
+        {
+            return db.Reviews;
+        }
+
+        // GET: api/Reviews/5
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult GetReview(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(review);
+        }
+
+        // PUT: api/Reviews/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutReview(int id, Review review)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != review.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(review).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReviewExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Reviews
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult PostReview(Review review)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Reviews.Add(review);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReviewExists(review.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = review.Id }, review);
+        }
+
+        // DELETE: api/Reviews/5
+        [ResponseType(typeof(Review))]
+        public IHttpActionResult DeleteReview(int id)
+        {
+            Review review = db.Reviews.Find(id);
+            if (review == null)
+            {
+                return NotFound();
+            }
+
+            db.Reviews.Remove(review);
+            db.SaveChanges();
+
+            return Ok(review);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReviewExists(int id)
+        {
+            return db.Reviews.Count(e => e.Id == id) > 0;
+        }
+
+        #endregion
+
+    }
+}
