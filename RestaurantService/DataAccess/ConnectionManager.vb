fbde8b301c6cc203955a2e9c4ad6233f4e3ba066
rjgiglio <rjgiglio@verizon.net>
2015-07-23 18:55:04 -0400

    new RestaurantReview service

---
+++ RestaurantService/DataAccess/ConnectionManager.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Configuration
+Imports System.Data
+Imports System.Data.SqlClient
+Friend NotInheritable Class ConnectionManager
+    ''' <summary>
+    ''' 07/22/2015 - rjg: Function GetConnection establishes, opens and returns connection to support T-SQL operations
+    ''' </summary>
+    ''' <remarks></remarks>
+
+    Public Shared Function GetConnection() As SqlConnection
+
+        Dim connectionString As String = "User ID=<username>;Password=<strong password>;Initial Catalog=Restaurant;Data Source=(local)"
+
+        ' create a new connection object
+        Dim connection As New SqlConnection(connectionString)
+
+        ' open and return connection
+        connection.Open()
+
+        Return connection
+
+    End Function
+
+End Class
+
---
+++ RestaurantService/readme.txt
@@ -0,0 +1,14 @@
+﻿This is a MS Windows Communication Foundation (WCF) service library 
+to provide an API to other components that serve platforms sharing
+information regarding customer reviews of restaurants.
+
+The intent is to capture or store the data into a MS SQL Server database.
+
+It is composed of an interface class defining the service and operation contracts
+used to provide the following data:
+
+Get a list of restaurants by city - returns list of restaurants for a specific city
+Post a restaurant that is not in the database
+Post a review for a restaurant
+Get of a list of reviews by user - returns list of all reviews ordered by user
+Delete a review
