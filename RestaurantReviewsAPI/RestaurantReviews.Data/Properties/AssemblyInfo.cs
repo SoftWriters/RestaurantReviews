4b40c490b32a03bbaf6bbb17b7a5a2c095c0dcd8
Jared Thompson <jthompson@brunnerworks.com>
2018-02-11 20:18:44 -0500

    Used Visual Studio autogenerated project to resolve naming mistake.

--- RestaurantReviewsAPI/RestaurantReviews.Data/Properties/AssemblyInfo.cs
+++ RestaurantReviewsAPI/RestaurantReviews.Data/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RestaurantReview.Data")]
+[assembly: AssemblyTitle("RestaurantReviews.Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RestaurantReview.Data")]
+[assembly: AssemblyProduct("RestaurantReviews.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b1724bd8-11a6-4c8e-8d40-2f69ddec62df")]
+[assembly: Guid("7a3ab718-b1fe-4429-b5d6-cde34b2c56f0")]
 
 // Version information for an assembly consists of the following four values:
 //
