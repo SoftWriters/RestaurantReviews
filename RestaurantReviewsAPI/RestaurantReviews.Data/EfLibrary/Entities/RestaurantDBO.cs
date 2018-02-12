7e237eee50aa76f7df69ceae005ecdcbd8749d9b
Jared Thompson <jthompson@brunnerworks.com>
2018-02-11 21:42:37 -0500

    Added EF code for data access.

---
+++ RestaurantReviewsAPI/RestaurantReviews.Data/EfLibrary/Entities/RestaurantDBO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Data.EfLibrary.Entities
+{
+    public class RestaurantDBO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public StateDBO State { get; set; }
+    }
+}
---
+++ RestaurantReviewsAPI/RestaurantReviews.Data/EfLibrary/RestaurantReviewsContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestaurantReviews.Data.EfLibrary.Entities;
+
+namespace RestaurantReviews.Data.EfLibrary
+{
+    public class RestaurantReviewsContext : DbContext
+    {
+        public IDbSet<UserDBO> Users { get; set; }
+        public IDbSet<StateDBO> States { get; set; }
+        public IDbSet<RestaurantDBO> Restaurants { get; set; }
+    }
+}
