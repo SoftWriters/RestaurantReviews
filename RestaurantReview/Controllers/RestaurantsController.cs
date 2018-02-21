d0b0a2fbd03f47ba24b39677a3597b1f023213c6
CheekyTorto <cheekytorto@gmail.com>
2018-02-21 13:14:36 -0500

    Add files via upload

---
+++ RestaurantReview/Controllers/RestaurantsController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantReview.Models;
+
+namespace RestaurantReview
+{
+    public class RestaurantsController : ApiController
+    {
+        private RestaurantReviewContext db = new RestaurantReviewContext();
+
+        // GET: api/Restaurants
+        public IQueryable<Restaurants> GetRestaurants()
+        {
+            return db.Restaurants;
+        }
+        // Get the restaurants name
+        // GET: api/Restaurants/5
+        [ResponseType(typeof(Restaurants))]
+        public async Task<IHttpActionResult> GetRestaurants(string name)
+        {
+            Restaurants restaurants = await db.Restaurants.FindAsync(name);
+            if (restaurants == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurants);
+        }
+        // Update the restaurant
+        // PUT: api/Restaurants/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutRestaurants(string name, Restaurants restaurants)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (name != restaurants.RestaurantName)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(restaurants).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                string id = null;
+                if (!RestaurantsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // Show the retaurants
+        // POST: api/Restaurants
+        [ResponseType(typeof(Restaurants))]
+        public async Task<IHttpActionResult> PostRestaurants(Restaurants restaurants)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Restaurants.Add(restaurants);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { name = restaurants.RestaurantName }, restaurants);
+        }
+        // Delete the restaurant
+        // DELETE: api/Restaurants/5
+        [ResponseType(typeof(Restaurants))]
+        public async Task<IHttpActionResult> DeleteRestaurants(string name)
+        {
+            Restaurants restaurants = await db.Restaurants.FindAsync(name);
+            if (restaurants == null)
+            {
+                return NotFound();
+            }
+
+            db.Restaurants.Remove(restaurants);
+            await db.SaveChangesAsync();
+
+            return Ok(restaurants);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RestaurantsExists(string name)
+        {
+            return db.Restaurants.Count(e => e.RestaurantName == name) > 0;
+        }
+    }
+}
---
+++ RestaurantReview/Models/RModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReview.Models
+{
+    public class Restaurants
+    {
+        /// <summary>
+        /// Internal Id of the Restaurant for the db
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// / Restaurant Name
+        /// </summary>
+        public string RestaurantName { get; set; }
+        /// <summary>
+        /// Restaurant Address
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Restaurant City
+        /// </summary>
+        public string City { get; set; }
+        /// <summary>
+        /// Restaurant State
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// Restaurant Zip Code
+        /// </summary>
+        public string Zip { get; set; }
+        /// <summary>
+        /// Restaurant Price Range. To be listed as "$" up to "$$$$$"
+        /// </summary>
+        public int PriceRange { get; set; }
+        /// <summary>
+        /// Restaurant Type - American,Japanese,Mexican, etc...
+        /// </summary>
+        public string RestaurantType { get; set; }
+        /// <summary>
+        /// Restaurant Family Friendly
+        /// </summary>
+        public string RestaurantFamilyFriendly { get; set; }
+        /// <summary>
+        /// Customer Reviews
+        /// </summary>
+        public string CustomerReview { get; set; }
+        /// <summary>
+        /// Customer Name, or userid, if entered...
+        /// </summary>
+        public string CustomerName { get; set; }
+        /// <summary>
+        /// Restaurant rating by Customers
+        /// </summary>
+        public string RestaurantRating { get; set; }
+        /// <summary>
+        /// Restaurant Phone Number
+        /// </summary>
+        public string RestaurantPhone { get; set; }
+        /// <summary>
+        /// Restaurant Web Site
+        /// </summary>
+        public string RestaurantWebSite { get; set; }
+    }
+}
