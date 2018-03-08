2ff15734ddbd2008d031bea4f109de1007f11077
Bharat Narra <bharatnarra@yahoo.com>
2018-03-08 10:36:14 -0500

    Restaurant Reviews Code Challenge

---
+++ RestaurantReviews/RestaurantReviews/Domain/Restaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReviews.Domain
+{
+    public class Restaurant
+    {
+        public int RestaurantId { get; set; }
+        public string RestaurantName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ RestaurantReviews/RestaurantReviews/Domain/Review.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantReviews.Domain
+{
+    public class Review
+    {
+        public int ReviewId { get; set; }
+        public int ReviewRestaurantId { get; set; } //Relationship between restaurant and review tables
+        public ReviewUser ReviewUser { get; set; }
+        public string ReviewContent { get; set; }
+        public bool Active { get; set; }
+
+    }
+
+    public class ReviewUser
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
