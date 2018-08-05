13e2bf4a02fa949d29af5d962653eb509ec94e64
maorlv <maorlv85@gmail.com>
2018-08-04 23:55:31 -0400

    added repositories for simple API methods

---
+++ RestaurantReviews/RestaurantReviews/Repositories/IReviewRepository.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantReviews.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.Repositories
+{
+    public interface IReviewRepository : IDisposable
+    {
+        Task<int> Add(Review review);
+        Task Delete(int id);
+        Task<Review> GetReview(int id);
+    }
+}
