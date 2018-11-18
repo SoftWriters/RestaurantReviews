5d32bd71b50796a730d88c95c615de4a68545e09
Chris Rickard <chris.rickard@opus.com>
2018-11-17 22:26:27 -0500

    Data Entities indicating by file name

---
+++ RestaurantReviewsApi.Model/Review.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RestaurantReviews.Model
+{
+    public class Review : IEntityBase
+    {
+        public Guid Id { get; set; }
+        public Guid RestaurantId { get; set; }
+        public string User { get; set; }
+        public string Text { get; set; }
+        public DateTime WhenCreated { get; set; } = DateTime.Now;
+        public string Url { get; set; }
+        public int Rating { get; set; }
+    }
+}
