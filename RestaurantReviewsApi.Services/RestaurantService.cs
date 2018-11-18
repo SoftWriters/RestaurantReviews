02cbe096bb19be9bd514a76dbcf08c6936e176fc
Chris Rickard <chris.rickard@opus.com>
2018-11-17 22:21:48 -0500

    Services files

---
+++ RestaurantReviewsApi.Services/RestaurantService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestaurantReviews.Data.Interfaces;
+using RestaurantReviews.Model;
+
+namespace RestaurantReviews.Services
+{
+    public interface IRestaurantService
+    {
+        List<Restaurant> GetAllRestaurants(string name = null);
+        List<Restaurant> GetRestaurantsByCity(string city);
+        Restaurant GetRestaurantById(Guid id);
+        void CreateRestaurant(Restaurant restaurant);
+        void DeleteRestaurant(Restaurant restaurant);
+        void UpdateRestaurant(Restaurant restaurant);
+        void SaveChanges();
+    }
+
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   A service for accessing restaurants information. </summary>
+    ///-------------------------------------------------------------------------------------------------
+
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary> RestaurantService Constructor </summary>
+        ///
+        /// <param name="restaurantRepository"> An instance of a restaurant repository. The framework uses
+        ///                                     dependency injection to supply the instance bewcause
+        ///                                     it was registered in the ConfigureServices method of the
+        ///                                     Startup class.</param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public RestaurantService(IRestaurantRepository restaurantRepository)
+        {
+            this._restaurantRepository = restaurantRepository;
+        }
+
+        #region IRestaurantService Members
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets all restaurants. </summary>
+        ///
+        /// <param name="name"> (Optional) The name of a single restaurant. </param>
+        /// <returns>   all restaurants as a List of Restarant objects. </returns>
+        ///-------------------------------------------------------------------------------------------------
+
+        public List<Restaurant> GetAllRestaurants(string name = null)
+        {
+            List<Restaurant> list;
+
+            if (string.IsNullOrEmpty(name))
+            {
+                list = _restaurantRepository.GetAll().ToList();
+            }
+
+            else
+            {
+                list = _restaurantRepository.GetAll().Where(x => x.Name == name).ToList();
+            }
+
+            return list;
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets all the restaurants by city. </summary>
+        ///
+        /// <param name="city"> The city that we are trying to find restaurants in. </param>
+        ///
+        /// <returns>   The restaurants by city. </returns>
+        /// 
+        ///  <remarks>   This could be more robust.  Searching for a city really requires geodata to do it right.
+        ///              See my design decisions for more details.</remarks>
+        ///
+        ///-------------------------------------------------------------------------------------------------
+
+        public List<Restaurant> GetRestaurantsByCity(string city)
+        {
+            List<Restaurant> restaurant = _restaurantRepository.GetRestaurantsByCity(city).ToList();
+            return restaurant;
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets restaurant by GUID identifier. </summary>
+        ///
+        /// <param name="id">   A Guid identifier. </param>
+        ///
+        /// <returns>   The restaurant identified by the argument. </returns>
+        /// 
+        /// <remarks> An endpoint using this method is unused in this exercise.</remarks>
+        ///-------------------------------------------------------------------------------------------------
+
+        public Restaurant GetRestaurantById(Guid id)
+        {
+            var restaurant = _restaurantRepository.GetById(id);
+            return restaurant;
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Creates a restaurant object and adds it to the data store. </summary>
+        ///
+        /// <param name="restaurant">   The restaurant object from which the restaurant will be created.</param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void CreateRestaurant(Restaurant restaurant)
+        {
+            _restaurantRepository.Add(restaurant);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Updates the restaurant in the data store described by the restaurant object. </summary>
+        ///
+        /// <param name="restaurant">   The restaurant object from which the restaurant will be updated. </param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void UpdateRestaurant(Restaurant restaurant)
+        {
+            _restaurantRepository.Update(restaurant);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Deletes the restaurant in the data store  described by the restaurant object. </summary>
+        ///
+        /// <param name="restaurant">   The restaurant object that will be deleted from a data store. </param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void DeleteRestaurant(Restaurant restaurant)
+        {
+            _restaurantRepository.Delete(restaurant);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Saves the changes that we make when creating or updating a database. </summary>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void SaveChanges()
+        {
+            _restaurantRepository.Save();
+        }
+
+        #endregion
+    }
+
+}
---
+++ RestaurantReviewsApi.Services/ReviewService.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestaurantReviews.Data.Interfaces;
+using RestaurantReviews.Model;
+
+namespace RestaurantReviews.Services
+{
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary>   Defines the contract for the ReviewService class </summary>
+    ///
+    /// <remarks>   This could be put in it's an file but I left it here for convenience</remarks>
+    ///-------------------------------------------------------------------------------------------------
+
+    public interface IReviewService
+    {
+        List<Review> GetReviews(string name = null);
+        Review GetReviewById(Guid id);
+        List<Review> GetReviewsByUser(string username);
+        void CreateReview(Review review);
+        void UpdateReview(Review review);
+        void DeleteReview(Review review);
+        void SaveChanges();
+    }
+
+    ///-------------------------------------------------------------------------------------------------
+    /// <summary> ReviewService Constructor </summary>
+    ///
+    /// <param name="reviewsRepository"> An instance of a review repository. The Web ApI 2 framework 
+    ///                                  uses dependency injection to supply the instance bewcause it was 
+    ///                                  registered in the ConfigureServices method of the Startup class.
+    ///                                  </param>
+    ///-------------------------------------------------------------------------------------------------
+
+    public class ReviewService : IReviewService
+    {
+        private readonly IReviewRepository _reviewsRepository;
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary> RestaurantService Constructor </summary>
+        ///
+        /// <param name="reviewsRepository"> An instance of a restaurant repository. The framework uses
+        ///                                  dependency injection to supply the instance bewcause
+        ///                                  it was registered in the ConfigureServices method of the
+        ///                                  Startup class.</param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public ReviewService(IReviewRepository reviewsRepository)
+        {
+            this._reviewsRepository = reviewsRepository;
+        }
+
+        #region IReviewService Members
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets all reviews in the data store. </summary>
+        ///
+        /// <param name="username"> (Optional) The name of a single restaurant. </param>
+        /// 
+        /// <returns>   all reviews as a List of Review objects. </returns>
+        ///-------------------------------------------------------------------------------------------------
+
+        public List<Review> GetReviews(string username = null)
+        {
+            if (string.IsNullOrEmpty(username))
+                return _reviewsRepository.GetAll().ToList();
+            else
+                return GetReviewsByUser(username).ToList();
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets a review as specified by a GUID identifier. </summary>
+        ///
+        /// <param name="id">   A Guid identifier. </param>
+        ///
+        /// <returns>   The review identified by the argument. </returns>
+        /// 
+        /// <remarks> An endpoint using this method is unused in this exercise.</remarks>
+        ///-------------------------------------------------------------------------------------------------
+
+        public Review GetReviewById(Guid id)
+        {
+            var review = _reviewsRepository.GetById(id);
+            return review;
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Gets reviews by user. </summary>
+        ///
+        /// <param name="username"> The username whose reviews we'll search for. </param>
+        ///
+        /// <returns>   The reviews written by a given user. </returns>
+        ///-------------------------------------------------------------------------------------------------
+
+        public List<Review> GetReviewsByUser(string username)
+        {
+            List<Review> reviews = _reviewsRepository.GetReviewsByUser(username).ToList();
+            return reviews;
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Creates a review object and adds it to the data store. </summary>
+        ///
+        /// <param name="review">   The review object from which the restaurant will be created.</param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void CreateReview(Review review)
+        {
+            _reviewsRepository.Add(review);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Updates the review in the data store described by the restaurant object. </summary>
+        ///
+        /// <param name="review">   The review object from which the restaurant will be updated.</param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void UpdateReview(Review review)
+        {
+            _reviewsRepository.Update(review);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Deletes the review in the data store  described by the restaurant object. </summary>
+        ///
+        /// <param name="review">   The review object that will be deleted from a data store. </param>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void DeleteReview(Review review)
+        {
+            _reviewsRepository.Delete(review);
+        }
+
+        ///-------------------------------------------------------------------------------------------------
+        /// <summary>   Saves the changes that we make when creating or updating a database. </summary>
+        ///-------------------------------------------------------------------------------------------------
+
+        public void SaveChanges()
+        {
+            _reviewsRepository.Save();
+        }
+        #endregion
+    }
+}
