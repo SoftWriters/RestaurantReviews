2fabd4e662f354878c319b0cc6e05dfd6a04d1a2
RockyBeMe <concept2@comcast.net>
2019-01-10 14:33:53 -0500

    Add files via upload

---
+++ RestaurantReviewsWebApi/Controllers/CitiesController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantReviewsEntities;
+
+namespace RestaurantReviewsWebApi.Controllers
+{
+    public class CitiesController : ApiController
+    {
+        private readonly RestaurantReviewsEntities.RestaurantReviewsEntities Db = new RestaurantReviewsEntities.RestaurantReviewsEntities();
+
+        // GET: api/Cities
+        public IQueryable<City> GetCities()
+        {
+            return Db.Cities;
+        }
+
+        // GET: api/Cities/5
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> GetCity(int id)
+        {
+            City city = await Db.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(city);
+        }
+
+        // PUT: api/Cities/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutCity(int id, City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+
+            Db.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                await Db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Cities
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> PostCity(City city)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Db.Cities.Add(city);
+
+            try
+            {
+                await Db.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (CityExists(city.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = city.Id }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [ResponseType(typeof(City))]
+        public async Task<IHttpActionResult> DeleteCity(int id)
+        {
+            City city = await Db.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            Db.Cities.Remove(city);
+            await Db.SaveChangesAsync();
+
+            return Ok(city);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CityExists(int id)
+        {
+            return Db.Cities.Count(e => e.Id == id) > 0;
+        }
+    }
+}
---
+++ RestaurantReviewsWebApi/Controllers/RestaurantsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantReviewsEntities;
+
+namespace RestaurantReviewsWebApi.Controllers
+{
+    public class RestaurantsController : ApiController
+    {
+        private readonly RestaurantReviewsEntities.RestaurantReviewsEntities Db = new RestaurantReviewsEntities.RestaurantReviewsEntities();
+
+        // GET: api/Restaurants
+        public IQueryable<Restaurant> GetRestaurants()
+        {
+            return Db.Restaurants;
+        }
+
+        // GET: api/Restaurants/5
+        [ResponseType(typeof(Restaurant))]
+        public async Task<IHttpActionResult> GetRestaurant(int id)
+        {
+            Restaurant restaurant = await Db.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(restaurant);
+        }
+
+        // PUT: api/Restaurants/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutRestaurant(int id, Restaurant restaurant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != restaurant.Id)
+            {
+                return BadRequest();
+            }
+
+            Db.Entry(restaurant).State = EntityState.Modified;
+
+            try
+            {
+                await Db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RestaurantExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Restaurants
+        [ResponseType(typeof(Restaurant))]
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Db.Restaurants.Add(restaurant);
+
+            try
+            {
+                await Db.SaveChangesAsync();
+            }
+            catch (DbUpdateException err)
+            {
+                if (RestaurantExists(restaurant.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = restaurant.Id }, restaurant);
+        }
+
+        // DELETE: api/Restaurants/5
+        [ResponseType(typeof(Restaurant))]
+        public async Task<IHttpActionResult> DeleteRestaurant(int id)
+        {
+            Restaurant restaurant = await Db.Restaurants.FindAsync(id);
+            if (restaurant == null)
+            {
+                return NotFound();
+            }
+
+            Db.Restaurants.Remove(restaurant);
+            await Db.SaveChangesAsync();
+
+            return Ok(restaurant);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool RestaurantExists(int id)
+        {
+            return Db.Restaurants.Count(e => e.Id == id) > 0;
+        }
+    }
+}
