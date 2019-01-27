1e188f2ba9643d8b90bf1b24fbc4cc3278b30fd2
Eric Kepes <eric@kepes.net>
2019-01-27 11:51:53 -0500

    Get the migration connection string from the config file

--- RestaurantReviews.Api/MigrationHandler.cs
+++ RestaurantReviews.Api/MigrationHandler.cs
@@ -1,5 +1,7 @@
 using System;
+using System.IO;
 using FluentMigrator.Runner;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RestaurantReviews.Api
@@ -16,13 +18,23 @@ namespace RestaurantReviews.Api
             }
         }
 
+        public static string GetConnectionString()
+        {
+            var root = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .Build();
+
+            return root.GetConnectionString("RestaurantDb");
+        }
+
         private static IServiceProvider CreateServices()
         {
             return new ServiceCollection()
                 .AddFluentMigratorCore()
                 .ConfigureRunner(rb => rb
                     .AddSqlServer()
-                    .WithGlobalConnectionString("Server=.;Database=RestaurantDB;User Id=RestaurantService;Password=MyReallyBadPassword1;")
+                    .WithGlobalConnectionString(GetConnectionString())
                     .ScanIn(typeof(RestaurantReviews.Api.MigrationHandler).Assembly).For.Migrations())
                 .AddLogging(lb => lb.AddFluentMigratorConsole())
                 .BuildServiceProvider(false);
