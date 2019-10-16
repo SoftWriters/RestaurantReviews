e2f7377e97a61234591489c1821a942ac906ddbf
unknown <tzviseliger@gmail.com>
2019-10-15 23:41:54 -0400

    initial commit. Restaurant/Review Models & Controller created

---
+++ RestaurantReview/RestaurantReview/Controllers/ReviewsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantReview.Controllers
+    {
+        [Route("api/[controller]")]
+        [ApiController]
+        public class ReviewsController : ControllerBase
+        {
+            // GET api/values
+            [HttpGet]
+            public ActionResult<IEnumerable<string>> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+
+            // POST api/values
+            [HttpPost]
+            public void Post([FromBody] string value)
+            {
+            }
+
+            // PUT api/values/5
+            [HttpPut("{id}")]
+            public void Put(int id, [FromBody] string value)
+            {
+            }
+
+            // DELETE api/values/5
+            [HttpDelete("{id}")]
+            public void Delete(int id)
+            {
+            }
+        }
+    }
