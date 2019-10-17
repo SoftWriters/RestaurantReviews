81bfb79a30bfcb54bc46e363e94b8fbd2efb5876
unknown <tzviseliger@gmail.com>
2019-10-16 20:37:38 -0400

    added db seed data file

--- RestaurantReview/RestaurantReview/DB/seedDB.sql
+++ RestaurantReview/RestaurantReview/DB/seedDB.sql
@@ -1,1 +1,66 @@
-﻿
+﻿INSERT INTO RESTAURANTS VALUES('Tonys', 'Boston');
+INSERT INTO RESTAURANTS VALUES('Max', 'Boston');
+INSERT INTO RESTAURANTS VALUES('Joint', 'New York');
+INSERT INTO RESTAURANTS VALUES('Hut', 'New York');
+INSERT INTO RESTAURANTS VALUES('The Cow', 'Pittsburgh');
+INSERT INTO RESTAURANTS VALUES('Curds', 'Pittsburgh');
+INSERT INTO RESTAURANTS VALUES('Dunkin Nonuts', 'Chicago');
+INSERT INTO RESTAURANTS VALUES('Flippin Pancakes', 'Chicago');
+INSERT INTO RESTAURANTS VALUES('Deli', 'Los Angeles');
+INSERT INTO RESTAURANTS VALUES('Pizzeria', 'Los Angeles');
+
+INSERT INTO USERS(UserName) VALUES('user1')
+INSERT INTO USERS(UserName) VALUES('user2')
+INSERT INTO USERS(UserName) VALUES('user3')
+INSERT INTO USERS(UserName) VALUES('user4')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(1, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(1, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(1, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(1, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(2, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(2, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(2, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(2, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(3, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(3, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(3, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(3, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(4, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(4, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(4, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(4, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(5, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(5, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(5, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(5, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(6, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(6, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(6, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(6, 4, 'Restaurant is subpar')
+
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(7, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(7, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(7, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(7, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(8, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(8, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(8, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(8, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(9, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(9, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(9, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(9, 4, 'Restaurant is subpar')
+
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(10, 1, 'Restaurant is awesome')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(10, 2, 'Restaurant is horrible')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(10, 3, 'Restaurant is ok')
+INSERT INTO REVIEWS (RestaurantId, UserId, ReviewText) VALUES(10, 4, 'Restaurant is subpar')
