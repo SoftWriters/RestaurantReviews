0e003586d4895633c82957f8d50d4fc4e29eed18
unknown <tzviseliger@gmail.com>
2019-10-28 02:45:34 -0400

    Saving changes

---
+++ RestaurantReview/RestaurantReviewTests/Controllers/RestaurantController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestaurantReview.Controllers;
+using RestaurantReview.Models;
+using RestaurantReview.Services;
+using System.Collections.Generic;
+using Xunit;
+
+namespace RestaurantReviewTests
+{
+    public class RestaurantController
+    {
+        public IConn connection = new Conn();
+
+        [Fact]
+        public void RestaurantsControllerTest()
+        {
+            //Arrange
+            var RC = new RestaurantsController(connection);
+
+            //Act
+            var result = RC.Get("Boston");
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
---
+++ RestaurantReview/RestaurantReviewTests/Models/RestaurantModel.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantReview.Models;
+using Xunit;
+
+namespace RestaurantReviewTests
+{
+    public class RestaurantModel
+    {
+        [Fact]
+        public void RestaurantModel_CityWithSpaceValid()
+        {
+            var sut = new Restaurant
+            {
+                City = "Sau Paulo"
+            };
+            Assert.True(sut.ValidateCity(), "should return 1 match");
+        }
+
+        [Fact]
+        public void RestaurantModel_CityWithNumberInvalid()
+        {
+            var sut = new Restaurant
+            {
+                City = "Sau Paulo1"
+            };
+            Assert.False(sut.ValidateCity(), "should return 0 matches");
+        }
+
+        [Fact]
+        public void RestaurantModel_CityWithSpecialCharsInvalid()
+        {
+            var sut = new Restaurant
+            {
+                City = "Sau Paulo!?!"
+            };
+            Assert.False(sut.ValidateCity(), "should return 0 matches");
+        }
+    }
+}
