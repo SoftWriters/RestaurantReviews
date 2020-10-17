1446725d7a9cfdc1bc25ad524f4d4d32d8a517f8
Zach Kearney <Zach.Kearney@careform.com>
2020-10-17 18:54:38 -0400

    Adding simple database model and .sqlproj with publish scripts.

---
+++ src/RestaurantReviewsApi/RestaurantReviewsApi.Database/dbo/Tables/Restaurant.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[Restaurant] (
+	[SystemId] INT IDENTITY (1, 1) NOT NULL ,
+	[RestaurantId] [uniqueidentifier] NOT NULL DEFAULT NEWID(),
+	[Name] NVARCHAR(100) NOT NULL,
+	[AddressLine1] NVARCHAR(100) NULL,
+	[AddressLine2] NVARCHAR(100) NULL,
+    [City] NVARCHAR(100) NULL,
+    [State] NVARCHAR(2) NULL,
+    [ZipCode] NVARCHAR(10) NULL,
+    [Phone] NVARCHAR(12) NULL,
+	[Website] NVARCHAR(100) NULL,
+	[Email] NVARCHAR(100) NULL,
+	[Description] NVARCHAR(500) NULL,
+    CONSTRAINT [PK_Restaurant_RestaurantId] PRIMARY KEY NONCLUSTERED ([RestaurantId] ASC));
+
+GO;
+
+CREATE UNIQUE CLUSTERED INDEX [CIX_Restaurant_SystemId] ON [dbo].[Restaurant](SystemId);
+
+GO;
---
+++ src/RestaurantReviewsApi/RestaurantReviewsApi.Database/dbo/Tables/Review.sql
@@ -0,0 +1,14 @@
+CREATE TABLE [dbo].[Review] (
+	[SystemId] INT IDENTITY (1, 1) NOT NULL ,
+	[ReviewId] UNIQUEIDENTIFIER NOT NULL DEFAULT NEWID(),
+	[RestaurantId] UNIQUEIDENTIFIER NOT NULL,
+	[UserName] NVARCHAR(100) NOT NULL,
+	[Rating] INT NOT NULL,
+	[Details] NVARCHAR(4000) NULL
+    CONSTRAINT [PK_Review_ReviewId] PRIMARY KEY NONCLUSTERED ([ReviewId] ASC));
+
+GO;
+
+CREATE UNIQUE CLUSTERED INDEX [CIX_Review_SystemId] ON [dbo].[Review](SystemId);
+
+GO;
