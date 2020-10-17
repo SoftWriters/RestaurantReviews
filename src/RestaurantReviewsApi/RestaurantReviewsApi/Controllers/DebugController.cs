bdd845e198c06626106f97e689e52bdbe3277cc8
Zach Kearney <Zach.Kearney@careform.com>
2020-10-17 17:51:42 -0400

    Initial commit of vs solution and a basic .net core api. - Added NLog. - Added Swagger. - Added DebugController. - Added ApiVersioning. - Added .gitignore

---
+++ src/RestaurantReviewsApi/RestaurantReviewsApi/Controllers/DebugController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantReviewsApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("[controller]")]
+    public class DebugController : ControllerBase
+    {
+        private readonly ILogger<DebugController> _logger;
+
+        public DebugController(ILogger<DebugController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return "pong";
+        }
+    }
+}
---
+++ src/RestaurantReviewsApi/RestaurantReviewsApi/Startup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NSwag;
+
+namespace RestaurantReviewsApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+            });
+
+            services.AddOpenApiDocument(config =>
+            {
+                config.DocumentName = "v1";
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "Restaurant Reviews Api";
+                    document.Schemes = new List<OpenApiSchema>() { OpenApiSchema.Http, OpenApiSchema.Https };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
