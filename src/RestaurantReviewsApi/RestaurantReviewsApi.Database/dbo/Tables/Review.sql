f46ebb84761c5d32af217e7150265ade760afeae
Zach Kearney <Zach.Kearney@careform.com>
2020-10-19 20:39:23 -0400

    Fixing indentation in sql scripts.

--- src/RestaurantReviewsApi/RestaurantReviewsApi.Database/dbo/Tables/Review.sql
+++ src/RestaurantReviewsApi/RestaurantReviewsApi.Database/dbo/Tables/Review.sql
@@ -6,7 +6,7 @@ CREATE TABLE [dbo].[Review] (
 	[Details] NVARCHAR(4000) NULL,
 	[IsDeleted] BIT NOT NULL DEFAULT 0,
 	[CreationDate] DATETIME NOT NULL DEFAULT GETUTCDATE(),
-    CONSTRAINT [PK_Review_ReviewId] PRIMARY KEY NONCLUSTERED ([ReviewId] ASC),
+	CONSTRAINT [PK_Review_ReviewId] PRIMARY KEY NONCLUSTERED ([ReviewId] ASC),
 	CONSTRAINT [FK_Review_RestaurantId] FOREIGN KEY([RestaurantId]) REFERENCES [dbo].[Restaurant]([RestaurantId]));
 GO;
 
