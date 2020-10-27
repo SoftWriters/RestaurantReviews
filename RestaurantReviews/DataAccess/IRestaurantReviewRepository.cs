2588456760731e3189419d2cc37e57647d918980
Joe Fallecker <jmfallecker@gmail.com>
2020-10-27 12:24:54 -0400

    add data access folder and IRestaurantReviewRepository interface

---
+++ RestaurantReviews/DataAccess/IRestaurantReviewRepository.cs
@@ -0,0 +1,24 @@
+﻿using RestaurantReviews.DomainModel;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RestaurantReviews.DataAccess
+{
+    public interface IRestaurantReviewRepository
+    {
+        IEnumerable<Restaurant> GetRestaurantsByCity(string city);
+        Task<IEnumerable<Restaurant>> GetRestaurantsByCityAsync(string city);
+
+        void AddRestaurant(Restaurant restaurant);
+        Task AddRestaurantAsync(Restaurant restaurant);
+
+        void AddReview(Review review);
+        Task AddReviewAsync(Review review);
+
+        IEnumerable<Review> GetReviewsByUser(User user);
+        Task<IEnumerable<Review>> GetReviewsByUsersAsync(User user);
+
+        void DeleteReview(Review review);
+        Task DeleteReviewAsync(Review review);
+    }
+}
