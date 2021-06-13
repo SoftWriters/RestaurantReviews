b2a727ad5f9d95fd8b691de6173528bc65b3f6cb
Jeremy Nelson <jeremy.nelson@tobiidynavox.com>
2021-06-12 21:44:26 -0400

    Added documentation

--- RestaurantReviews/RestaurantReviews.Web/Startup.cs
+++ RestaurantReviews/RestaurantReviews.Web/Startup.cs
@@ -43,6 +43,9 @@ namespace RestaurantReviews.Web
 
             // Add our Config object so it can be injected
             services.Configure<RestaurantReviewConfig>(Configuration.GetSection("RestaurantReviewConfig"));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +57,16 @@ namespace RestaurantReviews.Web
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Restaurant Reviews");
+            });
 
             app.UseRouting();
 
